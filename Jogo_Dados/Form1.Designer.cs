6c4811748f8d1f184f59ff90cd88affb27b6264f
Tatiane <tatianecruzpedro@hotmail.com>
2013-09-02 18:44:15 -0300

    PrimeiroEnvio

---
+++ Jogo_Dados/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace Jogo_Dados
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pb_dado1 = new System.Windows.Forms.PictureBox();
+            this.pb_dado2 = new System.Windows.Forms.PictureBox();
+            this.pb_dado3 = new System.Windows.Forms.PictureBox();
+            this.pb_dado4 = new System.Windows.Forms.PictureBox();
+            this.pb_dado5 = new System.Windows.Forms.PictureBox();
+            this.pb_dado6 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lb_jogador1 = new System.Windows.Forms.Label();
+            this.lb_jogador2 = new System.Windows.Forms.Label();
+            this.tb_aposta1 = new System.Windows.Forms.TextBox();
+            this.tb_aposta2 = new System.Windows.Forms.TextBox();
+            this.bt_gerar = new System.Windows.Forms.Button();
+            this.tb_resultado = new System.Windows.Forms.TextBox();
+            this.lb_nsorteio = new System.Windows.Forms.Label();
+            this.lb_ganhador = new System.Windows.Forms.Label();
+            this.tb_ganhador = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_dado1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_dado2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_dado3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_dado4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_dado5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_dado6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb_dado1
+            // 
+            this.pb_dado1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pb_dado1.Image = ((System.Drawing.Image)(resources.GetObject("pb_dado1.Image")));
+            this.pb_dado1.Location = new System.Drawing.Point(38, 221);
+            this.pb_dado1.Name = "pb_dado1";
+            this.pb_dado1.Size = new System.Drawing.Size(75, 70);
+            this.pb_dado1.TabIndex = 0;
+            this.pb_dado1.TabStop = false;
+            // 
+            // pb_dado2
+            // 
+            this.pb_dado2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pb_dado2.Image = ((System.Drawing.Image)(resources.GetObject("pb_dado2.Image")));
+            this.pb_dado2.Location = new System.Drawing.Point(151, 121);
+            this.pb_dado2.Name = "pb_dado2";
+            this.pb_dado2.Size = new System.Drawing.Size(75, 70);
+            this.pb_dado2.TabIndex = 1;
+            this.pb_dado2.TabStop = false;
+            // 
+            // pb_dado3
+            // 
+            this.pb_dado3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pb_dado3.Image = ((System.Drawing.Image)(resources.GetObject("pb_dado3.Image")));
+            this.pb_dado3.Location = new System.Drawing.Point(275, 47);
+            this.pb_dado3.Name = "pb_dado3";
+            this.pb_dado3.Size = new System.Drawing.Size(75, 70);
+            this.pb_dado3.TabIndex = 2;
+            this.pb_dado3.TabStop = false;
+            // 
+            // pb_dado4
+            // 
+            this.pb_dado4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pb_dado4.Image = ((System.Drawing.Image)(resources.GetObject("pb_dado4.Image")));
+            this.pb_dado4.Location = new System.Drawing.Point(408, 47);
+            this.pb_dado4.Name = "pb_dado4";
+            this.pb_dado4.Size = new System.Drawing.Size(75, 70);
+            this.pb_dado4.TabIndex = 3;
+            this.pb_dado4.TabStop = false;
+            // 
+            // pb_dado5
+            // 
+            this.pb_dado5.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pb_dado5.Image = ((System.Drawing.Image)(resources.GetObject("pb_dado5.Image")));
+            this.pb_dado5.Location = new System.Drawing.Point(528, 121);
+            this.pb_dado5.Name = "pb_dado5";
+            this.pb_dado5.Size = new System.Drawing.Size(75, 70);
+            this.pb_dado5.TabIndex = 4;
+            this.pb_dado5.TabStop = false;
+            // 
+            // pb_dado6
+            // 
+            this.pb_dado6.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pb_dado6.Image = ((System.Drawing.Image)(resources.GetObject("pb_dado6.Image")));
+            this.pb_dado6.Location = new System.Drawing.Point(647, 221);
+            this.pb_dado6.Name = "pb_dado6";
+            this.pb_dado6.Size = new System.Drawing.Size(75, 70);
+            this.pb_dado6.TabIndex = 5;
+            this.pb_dado6.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(202, 221);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(363, 80);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lb_jogador1
+            // 
+            this.lb_jogador1.AutoSize = true;
+            this.lb_jogador1.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_jogador1.Location = new System.Drawing.Point(32, 380);
+            this.lb_jogador1.Name = "lb_jogador1";
+            this.lb_jogador1.Size = new System.Drawing.Size(119, 34);
+            this.lb_jogador1.TabIndex = 7;
+            this.lb_jogador1.Text = "Jogador 1";
+            // 
+            // lb_jogador2
+            // 
+            this.lb_jogador2.AutoSize = true;
+            this.lb_jogador2.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_jogador2.Location = new System.Drawing.Point(32, 444);
+            this.lb_jogador2.Name = "lb_jogador2";
+            this.lb_jogador2.Size = new System.Drawing.Size(123, 34);
+            this.lb_jogador2.TabIndex = 8;
+            this.lb_jogador2.Text = "Jogador 2";
+            // 
+            // tb_aposta1
+            // 
+            this.tb_aposta1.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_aposta1.Location = new System.Drawing.Point(170, 380);
+            this.tb_aposta1.Name = "tb_aposta1";
+            this.tb_aposta1.Size = new System.Drawing.Size(56, 40);
+            this.tb_aposta1.TabIndex = 9;
+            // 
+            // tb_aposta2
+            // 
+            this.tb_aposta2.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_aposta2.Location = new System.Drawing.Point(170, 438);
+            this.tb_aposta2.Name = "tb_aposta2";
+            this.tb_aposta2.Size = new System.Drawing.Size(56, 40);
+            this.tb_aposta2.TabIndex = 10;
+            // 
+            // bt_gerar
+            // 
+            this.bt_gerar.BackColor = System.Drawing.Color.Red;
+            this.bt_gerar.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_gerar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.bt_gerar.Location = new System.Drawing.Point(324, 374);
+            this.bt_gerar.Name = "bt_gerar";
+            this.bt_gerar.Size = new System.Drawing.Size(112, 104);
+            this.bt_gerar.TabIndex = 11;
+            this.bt_gerar.Text = "Jogar";
+            this.bt_gerar.UseVisualStyleBackColor = false;
+            this.bt_gerar.Click += new System.EventHandler(this.bt_gerar_Click);
+            // 
+            // tb_resultado
+            // 
+            this.tb_resultado.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_resultado.ForeColor = System.Drawing.Color.Red;
+            this.tb_resultado.Location = new System.Drawing.Point(666, 374);
+            this.tb_resultado.Name = "tb_resultado";
+            this.tb_resultado.Size = new System.Drawing.Size(56, 40);
+            this.tb_resultado.TabIndex = 12;
+            // 
+            // lb_nsorteio
+            // 
+            this.lb_nsorteio.AutoSize = true;
+            this.lb_nsorteio.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_nsorteio.Location = new System.Drawing.Point(514, 380);
+            this.lb_nsorteio.Name = "lb_nsorteio";
+            this.lb_nsorteio.Size = new System.Drawing.Size(146, 34);
+            this.lb_nsorteio.TabIndex = 13;
+            this.lb_nsorteio.Text = "N° Sorteado";
+            // 
+            // lb_ganhador
+            // 
+            this.lb_ganhador.AutoSize = true;
+            this.lb_ganhador.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lb_ganhador.Location = new System.Drawing.Point(514, 444);
+            this.lb_ganhador.Name = "lb_ganhador";
+            this.lb_ganhador.Size = new System.Drawing.Size(124, 34);
+            this.lb_ganhador.TabIndex = 14;
+            this.lb_ganhador.Text = "Ganhador";
+            // 
+            // tb_ganhador
+            // 
+            this.tb_ganhador.Font = new System.Drawing.Font("Impact", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_ganhador.Location = new System.Drawing.Point(666, 444);
+            this.tb_ganhador.Name = "tb_ganhador";
+            this.tb_ganhador.Size = new System.Drawing.Size(56, 40);
+            this.tb_ganhador.TabIndex = 15;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            this.button1.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(263, 484);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 33);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Limpar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button2.Location = new System.Drawing.Point(390, 484);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 33);
+            this.button2.TabIndex = 17;
+            this.button2.Text = "Sair";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.tb_ganhador);
+            this.Controls.Add(this.lb_ganhador);
+            this.Controls.Add(this.lb_nsorteio);
+            this.Controls.Add(this.tb_resultado);
+            this.Controls.Add(this.bt_gerar);
+            this.Controls.Add(this.tb_aposta2);
+            this.Controls.Add(this.tb_aposta1);
+            this.Controls.Add(this.lb_jogador2);
+            this.Controls.Add(this.lb_jogador1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pb_dado6);
+            this.Controls.Add(this.pb_dado5);
+            this.Controls.Add(this.pb_dado4);
+            this.Controls.Add(this.pb_dado3);
+            this.Controls.Add(this.pb_dado2);
+            this.Controls.Add(this.pb_dado1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Tatiane / 1240609 - Jogo de Dados";
+            ((System.ComponentModel.ISupportInitialize)(this.pb_dado1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_dado2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_dado3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_dado4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_dado5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_dado6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb_dado1;
+        private System.Windows.Forms.PictureBox pb_dado2;
+        private System.Windows.Forms.PictureBox pb_dado3;
+        private System.Windows.Forms.PictureBox pb_dado4;
+        private System.Windows.Forms.PictureBox pb_dado5;
+        private System.Windows.Forms.PictureBox pb_dado6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lb_jogador1;
+        private System.Windows.Forms.Label lb_jogador2;
+        private System.Windows.Forms.TextBox tb_aposta1;
+        private System.Windows.Forms.TextBox tb_aposta2;
+        private System.Windows.Forms.Button bt_gerar;
+        private System.Windows.Forms.TextBox tb_resultado;
+        private System.Windows.Forms.Label lb_nsorteio;
+        private System.Windows.Forms.Label lb_ganhador;
+        private System.Windows.Forms.TextBox tb_ganhador;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Jogo_Dados/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Jogo_Dados
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //falta: dado sorteado aparecer!!!
+        //falta: inserir o n° do ganhador!!!!
+
+        private void bt_gerar_Click(object sender, EventArgs e)
+        {
+            Random numAleatorio = new Random();
+            
+            tb_resultado.Text = numAleatorio.Next(1,7).ToString();
+
+            if (tb_resultado == tb_aposta1)
+            {
+                tb_ganhador.Text = tb_aposta1.Text;
+            }
+            else if(tb_resultado == tb_aposta2)
+            {
+                tb_ganhador.Text = tb_aposta2.Text;
+            }
+                       
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tb_aposta1.Clear();
+            tb_aposta2.Clear();
+            tb_ganhador.Clear();
+            tb_resultado.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+    }
+}
